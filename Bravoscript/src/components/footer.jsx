25dd4e06d19c0c9fd583b7dd75e0069548892032
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-10-28 16:23:20 +0530

    Add Additional Files!

--- Bravoscript/src/components/footer.jsx
+++ Bravoscript/src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const home = () => {
+const footer = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const home = () => {
   )
 }
 
-export default home
+export default footer
