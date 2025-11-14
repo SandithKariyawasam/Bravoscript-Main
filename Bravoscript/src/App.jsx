3df0b0a4a757ef7fc9f5c6a53e185bbba25e5aaa
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-14 15:31:26 +0530

    Add Routes!

--- Bravoscript/src/App.jsx
+++ Bravoscript/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+
 import Header from './components/header'
 import Footer from './components/footer'
-import Home from './pages/home/home'
 
+import Home from './pages/home/home'
+import Details from './pages/details/details'
+import About from './pages/about/about'
+import Community from './pages/community/community'
+import Contact from './pages/contactus/contactus'
+import Icons from './pages/icons/icons'
+import Templates from './pages/templates/templates'
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/icons" element={<Icons />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/templates" element={<Templates />} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
