95273e48d5bf39115ade5b135f1fe751f9b74723
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-14 15:38:57 +0530

    Remove Video tag and Add HTML Iframe!

--- Bravoscript/src/pages/home/sections/section-six.jsx
+++ Bravoscript/src/pages/home/sections/section-six.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import '../home.css'
 
 import video from "../../../assets/videos/white.mp4"
 
 const SectionSix = () => {
+
+    const [html, setHtml] = useState("<h1>Hello Sandith</h1>");
+    const [css, setCss] = useState("h1 { color: red; text-align: center; }");
+    const [js, setJs] = useState("console.log('JS Running')");
+    const [srcDoc, setSrcDoc] = useState("");
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const source = `
+                <html>
+                  <head>
+                    <style>${css}</style>
+                  </head>
+                  <body>
+                    ${html}
+                    <script>
+                      try {
+                        ${js}
+                      } catch (err) {
+                        console.error(err);
+                      }
+                    </script>
+                  </body>
+                </html>
+              `;
+            setSrcDoc(source);
+        }, 300); // delay for auto-update
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js]);
+
+
     return (
         <>
             <div className="container-six">
@@ -12,39 +44,30 @@ const SectionSix = () => {
                     <div className="cardsrow">
 
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
 
@@ -52,42 +75,33 @@ const SectionSix = () => {
                     <div className="cardsrow">
 
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
                         <div className="cardsix">
-                            <video
-                                autoPlay
-                                muted
-                                loop
-                                playsInline
+                            <iframe
+                                srcDoc={srcDoc}
+                                title="{`project-${i}`}"
                                 className="card-video"
-                            >
-                                <source src={video} type="video/mp4" />
-                            </video>
+                                sandbox="allow-scripts allow-same-origin" // allow-same-origin if you need cookies/localStorage
+                            />
                             <span><i class="fa-duotone fa-solid fa-code"></i>Get code</span>
                         </div>
-                        
+
                     </div>
                 </div>
                 <button className='loadmore-btn'><i class="fa-solid fa-rocket"></i>Browse all components</button>
