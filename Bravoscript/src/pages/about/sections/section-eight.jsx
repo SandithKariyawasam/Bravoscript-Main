985a8a30b566e984ba20197a6985f72f16fd13a7
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-18 22:48:10 +0530

    About Page Section Eight Done!

---
+++ Bravoscript/src/pages/about/sections/section-eight.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const SectionEight = () => {
+    return (
+        <>
+            <div className="aboutone-container">
+                <h1>❓ FAQ</h1>
+
+                <ol>
+                    <li>
+                        <h4>Are all templates free?</h4>
+                        <p>Yes, completely free.</p>
+                    </li>
+                    <li>
+                        <h4>Can I use them for client projects?</h4>
+                        <p>Yes, commercial use is allowed.</p>
+                    </li>
+                    <li>
+                        <h4>Can I modify the code?</h4>
+                        <p>Absolutely.</p>
+                    </li>
+                    <li>
+                        <h4>Can I resell or redistribute your templates?</h4>
+                        <p>No, redistribution is not allowed.</p>
+                    </li>
+                    <li>
+                        <h4>Does BravoStudio collect personal data?</h4>
+                        <p>Only minimal data for analytics and communication.</p>
+                    </li>
+                    <li>
+                        <h4>Can I remove my data?</h4>
+                        <p>Yes, contact us anytime.</p>
+                    </li>
+                    <li>
+                        <h4>Can I upload resources to marketplaces?</h4>
+                        <p>No, that is not permitted.</p>
+                    </li>
+                    <li>
+                        <h4>Who owns BravoStudio?</h4>
+                        <p>BravoStudio is owned by Bravoscript, New Zealand.</p>
+                    </li>
+                    <li>
+                        <h4>Will more templates be added?</h4>
+                        <p>Yes, the library will regularly expand.</p>
+                    </li>
+                    <li>
+                        <h4>Do I need to give credit?</h4>
+                        <p>Not required but appreciated.</p>
+                    </li>
+                </ol>
+            </div>
+        </>
+    )
+}
+
+export default SectionEight
