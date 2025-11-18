13c290df9d6a2d8109e6068e9da5b0c327fbd56d
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-18 22:46:56 +0530

    About Page Section Two Done!

--- Bravoscript/src/pages/about/about.jsx
+++ Bravoscript/src/pages/about/about.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import SectionOne from './sections/section-one'
 import SectionTwo from './sections/section-two'
+import SectionThree from './sections/section-three'
+import SectionFour from './sections/section-four'
+import SectionFive from './sections/section-five'
+import SectionSix from './sections/section-six'
+import SectionSeven from './sections/section-seven'
+import SectionEight from './sections/section-eight'
 
 import './about.css'
 
@@ -10,6 +16,12 @@ const about = () => {
       <div className="about-container">
         <SectionOne />
         <SectionTwo />
+        <SectionThree />
+        <SectionFour />
+        <SectionFive />
+        <SectionSix />
+        <SectionSeven />
+        <SectionEight />
       </div>
     </>
   )
