49028aa00706da6e1c3f5a3898fae3ab4779a2e3
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-18 22:47:40 +0530

    About Page Section Six Done!

---
+++ Bravoscript/src/pages/about/sections/section-six.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const SectionSix = () => {
+    return (
+        <>
+            <div className="aboutone-container">
+                <h1>💬 REFUND & SUPPORT POLICY</h1>
+                <h3>Refund Policy</h3>
+                <p>
+                    BravoStudio provides free resources, so refunds do not apply.There are no paid items, subscriptions, or charges.
+                </p>
+
+                <h3>Support Policy</h3>
+                <p>We offer:</p>
+
+                <ul>
+                    <li>Basic support via email or contact form</li>
+                    <li>Best-effort troubleshooting</li>
+                    <li>No guaranteed response times</li>
+                    <li>No obligation for custom development or debugging your projects</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default SectionSix
