200cc1e4c5080cba5994645155f17309852d827d
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-11-30 11:34:26 +0530

    Update email.js

--- Bravoscript-backend/email.js
+++ Bravoscript-backend/email.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-const { db } = require('../firebase');
+const { db } = require('./firebase');
 
 router.post('/send-email', async (req, res) => {
     const { name, email, message } = req.body;
