f996daf52fb53010374bf1b3a16f34fd8977c6b1
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-12-02 20:47:52 +0530

    Upload Failed Deploy Files!

---
+++ Bravoscript/src/dashboard/admin/index.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import './index.css'
+import Dashboard from './dashboard/dashboard'
+import Upload from './upload/upload'
+import Sidenav from './components/sidenav'
+
+const Index = () => {
+  return (
+    <>
+
+      <div className="index">
+        <div className="side">
+          <Sidenav />
+        </div>
+        <div className="index-content">
+
+
+
+          <Routes>
+            <Route index element={<Dashboard />} />
+            <Route path="create" element={<Upload />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Index
