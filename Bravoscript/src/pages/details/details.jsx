82581dd08017f3daa820e0f1d2078c188b60dc25
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-12-03 00:32:38 +0530

    Update details.jsx

--- Bravoscript/src/pages/details/details.jsx
+++ Bravoscript/src/pages/details/details.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './details.css'
 import SectionOne from './sections/section-one'
 import SectionTwo from './sections/section-two'
 // import SectionThree from './sections/section-three'
