a00f6d8bd80352753ad3800a3c9898a990a3b15a
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-12-02 20:45:53 +0530

    Create firebase.js

---
+++ Bravoscript/src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNyKCyDS-SAY3pgWCTe3ZV56h6PbIXfkQ",
+  authDomain: "rising-pen-456515-h8.firebaseapp.com",
+  projectId: "rising-pen-456515-h8",
+  storageBucket: "rising-pen-456515-h8.firebasestorage.app",
+  messagingSenderId: "1002087414265",
+  appId: "1:1002087414265:web:9acbce5d7fa5d3ad6da528",
+  measurementId: "G-G6LLZ59Y3B"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
